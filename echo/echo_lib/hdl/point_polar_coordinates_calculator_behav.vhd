cafe8a1310e5831556c0bb3fa00524a0e05c102b
LeonelAguilera <la.aguilera@alumnos.upm.es>
2025-10-05 13:49:30 +0200

    Debugged a bit the polar coordinates calculator

--- echo/echo_lib/hdl/point_polar_coordinates_calculator_behav.vhd
+++ echo/echo_lib/hdl/point_polar_coordinates_calculator_behav.vhd
@@ -34,27 +34,27 @@ END point_polar_coordinates_calculator ;
 
 --
 ARCHITECTURE behav OF point_polar_coordinates_calculator IS
-  CONSTANT average_radius     : INTEGER := average_radius   <= (minor_radius + major_radius)/2;
+  CONSTANT average_radius     : INTEGER := (minor_radius + major_radius)/2;
   CONSTANT tap_minor_radius   : INTEGER := average_radius - (tap_height/2);
   CONSTANT tap_major_radius   : INTEGER := average_radius + (tap_height/2);
   
-  CONSTANT zero_bias_angle    : REAL := REAL(center_angle - (angle_amplitude / 2));
-  CONSTANT minor_bias_angle   : REAL := ARCTAN(REAL(tap_width) / (2 * REAL(tap_minor_radius))) / MATH_2_PI;
-  CONSTANT major_bias_angle   : REAL := ARCTAN(REAL(tap_width) / (2 * REAL(tap_major_radius))) / MATH_2_PI;
+  CONSTANT zero_bias_angle    : REAL := REAL(center_angle - (angle_amplitude / 2)) / 360.0;
+  CONSTANT minor_bias_angle   : REAL := ARCTAN(REAL(tap_width) / (2.0 * REAL(tap_minor_radius))) / MATH_2_PI;
+  CONSTANT major_bias_angle   : REAL := ARCTAN(REAL(tap_width) / (2.0 * REAL(tap_major_radius))) / MATH_2_PI;
   
-  CONSTANT point_a_bias_angle : INTEGER := INTEGER((255.0/REAL(angle_amplitude)) * (zero_bias_angle + major_bias_angle));
-  CONSTANT point_b_bias_angle : INTEGER := INTEGER((255.0/REAL(angle_amplitude)) * (zero_bias_angle - major_bias_angle));
-  CONSTANT point_c_bias_angle : INTEGER := INTEGER((255.0/REAL(angle_amplitude)) * (zero_bias_angle - minor_bias_angle));
-  CONSTANT point_d_bias_angle : INTEGER := INTEGER((255.0/REAL(angle_amplitude)) * (zero_bias_angle + minor_bias_angle));
+  CONSTANT point_a_bias_angle : INTEGER := INTEGER((255.0 * 360.0 / REAL(angle_amplitude)) * (zero_bias_angle + major_bias_angle));
+  CONSTANT point_b_bias_angle : INTEGER := INTEGER((255.0 * 360.0 / REAL(angle_amplitude)) * (zero_bias_angle - major_bias_angle));
+  CONSTANT point_c_bias_angle : INTEGER := INTEGER((255.0 * 360.0 / REAL(angle_amplitude)) * (zero_bias_angle - minor_bias_angle));
+  CONSTANT point_d_bias_angle : INTEGER := INTEGER((255.0 * 360.0 / REAL(angle_amplitude)) * (zero_bias_angle + minor_bias_angle));
   
   CONSTANT angle_padding : STD_LOGIC_VECTOR(INTEGER(REALMAX(CEIL(LOG2(360.0*255.0/REAL(angle_amplitude))), 8.0)) - 7 DOWNTO 0) := (OTHERS => '0');
 BEGIN
   radius <= TO_UNSIGNED(tap_major_radius, radius'length) WHEN point_selector(1) = '0' ELSE
             TO_UNSIGNED(tap_minor_radius, radius'length);
             
-  theta <= SIGNED(angle_padding & angle) + TO_SIGNED(point_a_bias_angle, theta'length) WHEN "00" ELSE -- Point A: Top left
-           SIGNED(angle_padding & angle) + TO_SIGNED(point_b_bias_angle, theta'length) WHEN "01" ELSE -- Point B: Top right
-           SIGNED(angle_padding & angle) + TO_SIGNED(point_c_bias_angle, theta'length) WHEN "10" ELSE -- Point C: Bottom right
-           SIGNED(angle_padding & angle) + TO_SIGNED(point_d_bias_angle, theta'length);               -- Point D: Bottom left
+  theta <= SIGNED(angle_padding & STD_LOGIC_VECTOR(angle)) + TO_SIGNED(point_a_bias_angle, theta'length) WHEN point_selector = "00" ELSE -- Point A: Top left
+           SIGNED(angle_padding & STD_LOGIC_VECTOR(angle)) + TO_SIGNED(point_b_bias_angle, theta'length) WHEN point_selector = "01" ELSE -- Point B: Top right
+           SIGNED(angle_padding & STD_LOGIC_VECTOR(angle)) + TO_SIGNED(point_c_bias_angle, theta'length) WHEN point_selector = "10" ELSE -- Point C: Bottom right
+           SIGNED(angle_padding & STD_LOGIC_VECTOR(angle)) + TO_SIGNED(point_d_bias_angle, theta'length);               -- Point D: Bottom left
 END ARCHITECTURE behav;
 
